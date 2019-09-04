810a43759b105f0e02aa9ef0ebbb2d2738fc4c58
littleonetwo <littleonetwo@gmail.com>
2019-09-03 20:49:03 -0500

    initial completed assignment

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,36 @@
 // };
 // myFunction();
 
+var myFunction = () => console.log("Function was invoked!");
+
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+var anotherFunction = param => console.log(param); //console .log added so you can actually see the work!
+
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+var add = (param,param2) => console.log(param+param2); //console .log added so you can actually see the work!
+
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+var subtract = (param1,param2) => console.log(param1-param2); //console .log added so you can actually see the work!
+
+subtract(1,2);
 
 
 // Stretch
